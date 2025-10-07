4937346d8a0ecc73e351312f35093bf5584ff78a
stormfiber <multiverseultra09@gmail.com>
2025-10-08 00:31:06 +0500

    Create imagemakers.go

---
+++ imagemakers.go
@@ -0,0 +1,39 @@
+package discard
+
+func (c *Client) QRCode(text string) (interface{}, error) {
+	return c.makeRequest("GET", "/api/maker/qrcode", map[string]interface{}{"text": text}, nil)
+}
+
+func (c *Client) QRTag(text string, size, color, logo string) (interface{}, error) {
+	params := map[string]interface{}{"text": text}
+	if size != "" {
+		params["size"] = size
+	}
+	if color != "" {
+		params["color"] = color
+	}
+	if logo != "" {
+		params["logo"] = logo
+	}
+	return c.makeRequest("GET", "/api/maker/qrtag", params, nil)
+}
+
+func (c *Client) TextToPic(text string) (interface{}, error) {
+	return c.makeRequest("GET", "/api/maker/ttp", map[string]interface{}{"text": text}, nil)
+}
+
+func (c *Client) TextAvatar(text, shape string) (interface{}, error) {
+	params := map[string]interface{}{"text": text}
+	if shape != "" {
+		params["shape"] = shape
+	}
+	return c.makeRequest("GET", "/api/maker/avatar", params, nil)
+}
+
+func (c *Client) CarbonImage(code, bg string) (interface{}, error) {
+	params := map[string]interface{}{"code": code}
+	if bg != "" {
+		params["bg"] = bg
+	}
+	return c.makeRequest("GET", "/api/maker/carbon", params, nil)
+}
