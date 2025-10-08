29902fcb940775f528a7ceb7f24e310dce23fe37
stormfiber <multiverseultra09@gmail.com>
2025-10-08 13:17:43 +0500

    Update shortener.go

--- shortener.go
+++ shortener.go
@@ -12,8 +12,28 @@ func (c *Client) ShortenTiny(url string) (interface{}, error) {
 	return c.makeRequest("GET", "/api/short/tiny", map[string]interface{}{"url": url}, nil)
 }
 
-func (c *Client) ShortenBitly(url string) (interface{}, error) {
-	return c.makeRequest("GET", "/api/short/bitly", map[string]interface{}{"url": url}, nil)
+func (c *Client) ShortenTinycc(url string) (interface{}, error) {
+	return c.makeRequest("GET", "/api/short/tinycc", map[string]interface{}{"url": url}, nil)
+}
+
+func (c *Client) ShortenReurl(url string) (interface{}, error) {
+	return c.makeRequest("GET", "/api/short/reurl", map[string]interface{}{"url": url}, nil)
+}
+
+func (c *Client) ShortenSurl(url string) (interface{}, error) {
+	return c.makeRequest("GET", "/api/short/surl", map[string]interface{}{"url": url}, nil)
+}
+
+func (c *Client) ShortenVurl(url string) (interface{}, error) {
+	return c.makeRequest("GET", "/api/short/vurl", map[string]interface{}{"url": url}, nil)
+}
+
+func (c *Client) ShortenVgd(url string) (interface{}, error) {
+	return c.makeRequest("GET", "/api/short/vgd", map[string]interface{}{"url": url}, nil)
+}
+
+func (c *Client) ShortenClean(url string) (interface{}, error) {
+	return c.makeRequest("GET", "/api/short/clean", map[string]interface{}{"url": url}, nil)
 }
 
 func (c *Client) Unshort(url string) (interface{}, error) {
