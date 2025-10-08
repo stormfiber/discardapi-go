c1cefb86c348273891b5824618cb874988b5ab7b
stormfiber <multiverseultra09@gmail.com>
2025-10-08 19:42:47 +0500

    Update apk.go

--- apk.go
+++ apk.go
@@ -4,7 +4,7 @@ func (c *Client) Android1(query string) (interface{}, error) {
 	return c.makeRequest("GET", "/api/apk/search/android1", map[string]interface{}{"query": query}, nil)
 }
 
-func (c *Client) dlAndroid1(url string) (interface{}, error) {
+func (c *Client) DAndroid1(url string) (interface{}, error) {
 	return c.makeRequest("GET", "/api/apk/dl/android1", map[string]interface{}{"url": url}, nil)
 }
 
@@ -12,7 +12,7 @@ func (c *Client) ApkMirror(query string) (interface{}, error) {
 	return c.makeRequest("GET", "/api/apk/search/apkmirror", map[string]interface{}{"query": query}, nil)
 }
 
-func (c *Client) dlApkMirror(url string) (interface{}, error) {
+func (c *Client) DApkMirror(url string) (interface{}, error) {
 	return c.makeRequest("GET", "/api/apk/dl/apkmirror", map[string]interface{}{"url": url}, nil)
 }
 
@@ -20,7 +20,7 @@ func (c *Client) ApkPure(query string) (interface{}, error) {
 	return c.makeRequest("GET", "/api/apk/search/apkpure", map[string]interface{}{"query": query}, nil)
 }
 
-func (c *Client) dlApkPure(url string) (interface{}, error) {
+func (c *Client) DApkPure(url string) (interface{}, error) {
 	return c.makeRequest("GET", "/api/apk/dl/apkpure", map[string]interface{}{"url": url}, nil)
 }
 
@@ -28,7 +28,7 @@ func (c *Client) PlayStore(query string) (interface{}, error) {
 	return c.makeRequest("GET", "/api/apk/search/playstore", map[string]interface{}{"query": query}, nil)
 }
 
-func (c *Client) dlPlayStore(url string) (interface{}, error) {
+func (c *Client) DPlayStore(url string) (interface{}, error) {
 	return c.makeRequest("GET", "/api/apk/dl/playstore", map[string]interface{}{"url": url}, nil)
 }
 
